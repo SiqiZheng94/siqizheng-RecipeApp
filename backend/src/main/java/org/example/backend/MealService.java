882f530797f44b6341a423bf6fc64da3f674b599
Sergej Jaudszims <redtape9688@gmail.com>
2023-12-13 14:26:05 +0100

    add GetAll

--- backend/src/main/java/org/example/backend/MealService.java
+++ backend/src/main/java/org/example/backend/MealService.java
@@ -1,4 +1,16 @@
 package org.example.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class MealService {
+
+    @Autowired
+    private MealRepo repo;
+
+    public List<MealRecord> getAllMeals() {
+        return repo.findAll();
+    }
 }
