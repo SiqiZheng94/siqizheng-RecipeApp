623c9ca2162521fb35899ddb3e3b4c24b607b3cd
magihoefkens <magi.hoefkens@outlook.de>
2023-12-13 21:26:08 +0100

    first fetch the data to the frontend. Needed to change the endpoint to /api/meals because of the proxy target in vite.config.ts

--- backend/src/main/java/org/example/backend/MealController.java
+++ backend/src/main/java/org/example/backend/MealController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/meals")
+@RequestMapping("/api/meals")
 public class MealController {
 
     @Autowired
