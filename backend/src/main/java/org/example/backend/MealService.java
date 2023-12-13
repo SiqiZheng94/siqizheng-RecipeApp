014ea444ef0956f6e86d7c013aacbc7a5486b2ef
magihoefkens <magi.hoefkens@outlook.de>
2023-12-13 18:35:41 +0100

    RequiredArgsConstructor added for MealService and MealController and made repo and service final

--- backend/src/main/java/org/example/backend/MealService.java
+++ backend/src/main/java/org/example/backend/MealService.java
@@ -1,14 +1,16 @@
 package org.example.backend;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MealService {
 
     @Autowired
-    private MealRepo repo;
+    private final MealRepo repo;
 
     public List<MealRecord> getAllMeals() {
         return repo.findAll();
