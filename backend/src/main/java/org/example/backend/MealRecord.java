af2a38b1bb1bd6fdbcd5aa6e3694c8f466963c9b
Ivan <i85chiosa@gmail.com>
2023-12-13 13:00:50 +0100

    mealRecord update

--- backend/src/main/java/org/example/backend/MealRecord.java
+++ backend/src/main/java/org/example/backend/MealRecord.java
@@ -1,5 +1,40 @@
 package org.example.backend;
 
-public record MealRecord() {
-    //Hello
+import lombok.With;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@With
+//@Document("meals")
+public record MealRecord(
+
+        String _id,
+        String idMeal,
+        String strMeal,
+        String strCategory,
+        String strArea,
+        String strInstructions,
+        String strMealThumb,
+        String strTags,
+        String strIngredient1,
+        String strIngredient2,
+        String strIngredient3,
+        String strIngredient4,
+        String strIngredient5,
+        String strIngredient6,
+        String strIngredient7,
+        String strIngredient8,
+        String strIngredient9,
+        String strIngredient10,
+        String strMeasure1,
+        String strMeasure2,
+        String strMeasure3,
+        String strMeasure4,
+        String strMeasure5,
+        String strMeasure6,
+        String strMeasure7,
+        String strMeasure8,
+        String strMeasure9,
+        String strMeasure10
+
+            )  {
 }
