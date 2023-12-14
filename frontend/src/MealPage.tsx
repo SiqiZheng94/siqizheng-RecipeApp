e2955008d83f9c9f5f4fba49dbfa7eb7579529bd
siqizheng <cn.siqizheng@gmail.com>
2023-12-14 15:04:26 +0100

    navbar.tsx & .css

--- frontend/src/MealPage.tsx
+++ frontend/src/MealPage.tsx
@@ -1,4 +1,5 @@
 import {Meal} from "./Meal.ts";
+import Navbar from "./components/Navbar.tsx";
 
 type MealPageProps={
     meals:Meal[],
@@ -8,10 +9,9 @@ export default function MealPage(props:MealPageProps){
     return (
         <>
             <h1>Welcome to the Meal App</h1>
-            <div  className={"container"}>
+            <Navbar/>
+            <div  className={"meal-container"}>
                 {props.meals.map((meal:Meal) => (
-
-
                             <div className={"meal-card"}>
                                 {meal.strMealThumb && (
                                     <img className={"meal-picture"}
@@ -21,8 +21,6 @@ export default function MealPage(props:MealPageProps){
                                 )}
                                 <p className={"meal-introduction"} s>{meal.strMeal}</p>
                             </div>
-
-
                     )
                 )}
             </div>
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar (){
+    return (
+        <div className={"navbar container"}>
+            <a href={"#!"} className={"logo"}>Best<span>Recipe</span>App</a>
+            <div className={"nav-links"}>
+                <a href={"#!"}>Home</a>
+                <a href={"#!"}>Recipes</a>
+            </div>
+        </div>
+    )
+}
