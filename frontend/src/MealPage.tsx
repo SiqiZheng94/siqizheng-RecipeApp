a83c0337f17eae3f1eeaa1ad5431cc599cf0261a
siqizheng <cn.siqizheng@gmail.com>
2023-12-14 12:44:28 +0100

    design meal-card

--- frontend/src/MealPage.tsx
+++ frontend/src/MealPage.tsx
@@ -8,23 +8,24 @@ export default function MealPage(props:MealPageProps){
     return (
         <>
             <h1>Welcome to the Meal App</h1>
-            <ul>
+            <div  className={"container"}>
                 {props.meals.map((meal:Meal) => (
 
-                        <div key={meal.idMeal}>
-                            {meal.strMealThumb && (
-                                <img
-                                    src={meal.strMealThumb}
-                                    alt={meal.strMeal}
-                                    style={{ maxWidth: "200px", maxHeight: "200px" }}
-                                />
-                            )}
-                            <div>{meal.strMeal},{meal.strCategory}</div>
-                        </div>
+
+                            <div className={"meal-card"}>
+                                {meal.strMealThumb && (
+                                    <img className={"meal-picture"}
+                                        src={meal.strMealThumb}
+                                        alt={meal.strMeal}
+                                    />
+                                )}
+                                <p>{meal.strMeal},{meal.strCategory}</p>
+                            </div>
+
 
                     )
                 )}
-            </ul>
+            </div>
 
         </>
     )
