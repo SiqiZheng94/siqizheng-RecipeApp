0eb093bb811b0e283eb87d3a5b4a03965f2cad27
siqizheng <cn.siqizheng@gmail.com>
2023-12-20 17:48:28 +0100

    add edit & delete & manage buttons

--- frontend/src/pages/AllMealPage.tsx
+++ frontend/src/pages/AllMealPage.tsx
@@ -3,30 +3,49 @@ import {useNavigate} from "react-router-dom";
 import SearchBar from '../components/SearchBar';
 import { useState } from 'react';
 
+
 type MealPageProps = {
     meals: Meal[],
 }
 export default function AllMealPage(props: MealPageProps) {
     const navigate = useNavigate()
     const [meals, setMeals] = useState(props.meals);
+    const [manage, setManage] = useState<boolean>(false)
+    function changeManageState() {
+        if (!manage){
+            setManage(true)
+        } else {
+            setManage(false)
+        }
+    }
 
     return (
-        <div><SearchBar setMeals={setMeals} />
-        <div className={"meal-container"}>
+        <div>
+            <div className={"management"}>
+                <button onClick={changeManageState}><span>Manage</span></button>
+                <SearchBar setMeals={setMeals} />
+            </div>
 
-            {meals.map((meal: Meal) => (
-                    <div className={"meal-card"} key={meal.idMeal} onClick={()=>navigate("/recipe/"+meal._id)}>
-                        {meal.strMealThumb && (
-                            <img className={"meal-picture"}
-                                 src={meal.strMealThumb}
-                                 alt={meal.strMeal}
-                            />
-                        )}
-                        <p className={"meal-introduction"}>{meal.strMeal}</p>
-                    </div>
-                )
-            )}
-        </div>
+            <div className={"meal-container"}>
+
+                {meals.map((meal: Meal) => (
+                        <div className={"meal-card"} key={meal.idMeal} onClick={()=>navigate("/recipe/"+meal._id)}>
+                            {meal.strMealThumb && (
+                                <img className={"meal-picture"}
+                                     src={meal.strMealThumb}
+                                     alt={meal.strMeal}
+                                />
+                            )}
+                            {manage &&
+                            <div className={"two-buttons"}>
+                                <button><span>Edit</span></button>
+                                <button><span>Delete</span></button>
+                            </div>}
+                            <p className={"meal-introduction"}>{meal.strMeal}</p>
+                        </div>
+                    )
+                )}
+            </div>
         </div>
     )
 }
